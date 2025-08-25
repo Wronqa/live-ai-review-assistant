6d3869130425cc881c6f80fb7ba769cfbcbb2086
Wronqa <kuba.wrona@onet.pl>
2025-08-25 21:19:09 +0200

    feat(network): add outputs for public subnets ids and security group id

--- infra/live/modules/network/outputs.tf
+++ infra/live/modules/network/outputs.tf
@@ -1,1 +1,3 @@
 output "vpc_id"            { value = aws_vpc.this.id }
+output "public_subnet_ids" { value = [for s in aws_subnet.public : s.id] }
+output "ecs_sg_id"         { value = aws_security_group.ecs_tasks.id }
