f333cf4595f24a4aba9140325550ab570bc44c60
Wronqa <kuba.wrona@onet.pl>
2025-09-11 22:38:32 +0200

    feat(s3_artifacts): enhance S3 bucket configuration with ownership controls, lifecycle rules, and secure policies

--- infra/live/modules/s3_artifacts/main.tf
+++ infra/live/modules/s3_artifacts/main.tf
@@ -3,8 +3,17 @@ resource "random_id" "suffix" {
 }
 
 resource "aws_s3_bucket" "this" {
-  bucket = "${var.name_prefix}-${random_id.suffix.hex}"
-  tags   = var.tags
+  bucket = local.name
+  tags   = local.tags
+  force_destroy = var.force_destroy
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "this" {
@@ -26,7 +35,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = var.sse_algorithm
+      kms_master_key_id = var.sse_algorithm == "aws:kms" ? var.kms_key_id : null
     }
   }
 }
@@ -34,12 +44,69 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 resource "aws_s3_bucket_lifecycle_configuration" "this" {
   bucket = aws_s3_bucket.this.id
   rule {
-    id     = "expire-${var.lifecycle_days}d"
+    id     = "transition-${var.transition_days}d"
     status = "Enabled"
-    expiration {
-      days = var.lifecycle_days
-    }
 
     filter {}
+ 
+     transition {
+      days          = var.transition_days
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = var.transition_days
+      storage_class   = "GLACIER"
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
   }
 }
+
+data "aws_iam_policy_document" "secure" {
+  statement {
+    sid     = "DenyInsecureTransport"
+    effect  = "Deny"
+    actions = ["s3:*"]
+    principals { 
+      type = "AWS"
+      identifiers = ["*"] 
+    }
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*"
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+
+  statement {
+    sid     = "RequireSSE"
+    effect  = "Deny"
+    actions = ["s3:PutObject"]
+    principals { 
+      type = "AWS"
+      identifiers = ["*"] 
+    }
+    resources = ["${aws_s3_bucket.this.arn}/*"]
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = [var.sse_algorithm]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "secure" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.secure.json
+}
+
+
+
