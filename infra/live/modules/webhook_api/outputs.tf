de41a9299f40b8d00db10dd28d38c3089c94c37e
Wronqa <kuba.wrona@onet.pl>
2025-09-15 19:52:44 +0200

    feat(webhook_api): add role_name output for IAM role in webhook API module

--- infra/live/modules/webhook_api/outputs.tf
+++ infra/live/modules/webhook_api/outputs.tf
@@ -1,2 +1,3 @@
 output "url"         { value = aws_apigatewayv2_api.http.api_endpoint }
 output "function_arn"{ value = aws_lambda_function.fn.arn }
+output "role_name" {value = aws_iam_role.lambda_role.name}
