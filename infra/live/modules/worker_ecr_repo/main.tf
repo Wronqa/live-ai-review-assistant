ea8145289f5687515a3eb456e1c524d7848a723c
Wronqa <kuba.wrona@onet.pl>
2025-09-15 21:23:56 +0200

    refactor(worker_ecr_repo): update ECR repository configuration to use local variables and improve clarity

--- infra/live/modules/worker_ecr_repo/main.tf
+++ infra/live/modules/worker_ecr_repo/main.tf
@@ -1,9 +1,19 @@
 resource "aws_ecr_repository" "this" {
-  name                 = var.name
-  image_tag_mutability = "MUTABLE"
-  image_scanning_configuration { scan_on_push = true }
-  encryption_configuration { encryption_type = "AES256" } 
-  tags = var.tags
+  name                 = local.name
+  image_tag_mutability = var.image_tag_mutability
+
+  image_scanning_configuration {
+     scan_on_push = var.scan_on_push 
+  }
+
+  encryption_configuration {
+    encryption_type = var.encryption_type
+    kms_key         = var.encryption_type == "KMS" ? var.kms_key_arn : null
+  }
+
+  force_delete = var.force_delete
+
+  tags = local.tags
 }
 
 resource "aws_ecr_lifecycle_policy" "keep_last_10" {
