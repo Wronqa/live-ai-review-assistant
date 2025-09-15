573849fe7b94597802b506199eac9dd874b60fac
Wronqa <kuba.wrona@onet.pl>
2025-09-15 22:52:06 +0200

    feat(review_sqs): add descriptions to SQS queue output variables

--- infra/live/modules/review_sqs/outputs.tf
+++ infra/live/modules/review_sqs/outputs.tf
@@ -1,3 +1,14 @@
-output "queue_url" { value = aws_sqs_queue.review.url }
-output "queue_arn" { value = aws_sqs_queue.review.arn }
-output "dlq_url"   { value = aws_sqs_queue.dlq.url }
+output "queue_url" {
+  description = "URL of the review SQS queue."
+  value       = aws_sqs_queue.review.url
+}
+
+output "queue_arn" {
+  description = "ARN of the review SQS queue."
+  value       = aws_sqs_queue.review.arn
+}
+
+output "dlq_url" {
+  description = "URL of the dead-letter queue (DLQ) for the review SQS queue."
+  value       = aws_sqs_queue.dlq.url
+}
