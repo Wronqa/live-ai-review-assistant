7d8fe85c2725c29bc9dfd1e55d3c2854c1a544cb
Wronqa <kuba.wrona@onet.pl>
2025-09-18 21:13:52 +0200

    refactor(secrets): correct variable usage for names and update tag naming format

--- infra/live/modules/secrets/main.tf
+++ infra/live/modules/secrets/main.tf
@@ -1,8 +1,8 @@
 resource "aws_secretsmanager_secret" "this" {
-  for_each = toset(local.names)
+  for_each = toset(var.names)
 
   name = each.value
-  tags = merge(local.tags, { Name = "${local.name}-${each.value}", Component = "secret" })
+  tags = merge(local.tags, { Name = "each.value", Component = "secret" })
 
   recovery_window_in_days = 7
 }
