bf86970cb60af5f2e9dcfa60ae376787515b1b92
Wronqa <kuba.wrona@onet.pl>
2025-09-18 21:25:16 +0200

    refactor(task_secret_access): remove unused resources and variables for cleanup

--- infra/live/modules/task_secret_access/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  name = var.name
-  tags = merge(var.tags, { ManagedBy = "terraform" })
-}
--- infra/live/modules/task_secret_access/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "role_name"    { type = string }           
-variable "secret_arns" { type = list(string) }     
-variable "name"        { type = string }           
-variable "tags"        { 
-    type = map(string) 
-    default = {} 
-}
