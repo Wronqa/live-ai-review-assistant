fdd6e64039438fcf4a627a136393ec0d455b5a06
Wronqa <kuba.wrona@onet.pl>
2025-09-23 20:25:12 +0200

    refactor(infra): remove pipes_sqs_to_ecs module and associated files

--- infra/live/modules/pipes_sqs_to_ecs/locals.tf
+++
@@ -1,5 +0,0 @@
-
-locals {
-  name                 = var.name
-  tags                 = merge(var.tags, { ManagedBy = "terraform" })
-}
--- infra/live/modules/pipes_sqs_to_ecs/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "pipe_arn"  { value = aws_pipes_pipe.this.arn }
-output "pipe_name" { value = aws_pipes_pipe.this.name }
-output "role_arn"  { value = aws_iam_role.this.arn }
